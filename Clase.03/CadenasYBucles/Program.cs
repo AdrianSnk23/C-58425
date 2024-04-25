dcee5cd4c87d13438ce64252f88f5f2376cd7f56
Adrian Rodriguez <106604113+AdrianSnk23@users.noreply.github.com>
2024-04-25 10:06:24 -0300

    Clase 3
    Aprendemos sobre bucles y manejo de cadenas

---
+++ Clase.03/CadenasYBucles/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string cadenaNula = null;
+string cadenaVacia = string.Empty;
+string cadenaSaludo = "Hola, soy una cadena...";
+
+Console.WriteLine(cadenaSaludo + cadenaNula + cadenaVacia);
+
+if(cadenaNula == cadenaVacia)
+{
+    Console.WriteLine("Las cadenas son iguales");
+}
+else
+{
+    Console.WriteLine("Las cadenas no son iguales!");
+}
+
+//Console.WriteLine(cadenaNula.Length);
+Console.WriteLine(cadenaVacia.Length);
+Console.WriteLine(cadenaSaludo.Length);
+
+string mensaje = "Voy a desarrollar en C#";
+
+Console.WriteLine("Mensaje en minuscula: " + mensaje.ToLower());
+Console.WriteLine("Mensaje en minuscula: " + mensaje.ToUpper());
+
+int numeroEntero = 23;
+double numeroConDecimales = 1.88;
+bool booleano = true;
+
+Console.WriteLine("La edad de Luis es: " + numeroEntero.ToString() + " " + numeroConDecimales.ToString() + " " + booleano.ToString());
+
+string cadena1 = "Soy una cadena ";
+string cadena2 = "Soy una cadena";
+
+if (cadena1.Equals(cadena2))
+{
+    Console.WriteLine("La cadena uno ES IGUAL a la cadena 2");
+}
+else
+{
+    Console.WriteLine("La cadena uno NO ES IGUAL a la cadena 2");
+}
+
+if (cadena1.Contains(cadena2))
+{
+    Console.WriteLine("La cadena uno CONTIENE a la cadena 2");
+}
+else
+{
+    Console.WriteLine("La cadena uno NO contiene a la cadena 2");
+}
+
+Console.WriteLine("Ingrese un numero para sumar:");
+
+string cadena = Console.ReadLine();
+
+double constante  = 0.5;
+
+double numeroEnCadenaConvertidoADouble;
+
+double sumaConNumeroDouble;
+
+if (string.IsNullOrEmpty(cadena))
+{
+    Console.WriteLine("La cadena no se puede parsear ya que esta vacia.");
+}
+else
+{
+    numeroEnCadenaConvertidoADouble = double.Parse(cadena);
+    sumaConNumeroDouble = numeroEnCadenaConvertidoADouble + constante;
+    Console.WriteLine("El resultado es: " + sumaConNumeroDouble);
+}
---
+++ Clase.03/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int CantidadDeNotas = 0;
+double promedioNotas  = 0;
+double nota;
+double sumaNotas = 0;
+
+Console.WriteLine("Ingrese la cantidad de notas a calcular: ");
+CantidadDeNotas = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= CantidadDeNotas; i++)
+{
+    Console.WriteLine("Ingrese la nota: ");
+    nota = double.Parse(Console.ReadLine());
+    sumaNotas = sumaNotas + nota;
+}
+
+promedioNotas = sumaNotas / CantidadDeNotas;
+
+Console.WriteLine("El promedio de los alumnos cargados es: {0}", promedioNotas);
